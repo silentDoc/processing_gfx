51e543492387c75223b4d0ce0d0c5ffbf6d9a95a
silentDoc <ramon78t@yahoo.es>
2024-03-22 20:47:28 +0100

    removed renamed stuff

--- PerlinNoiseTheory/matlab/perlinAmplitudeSample.m
+++
@@ -1,9 +0,0 @@
-xs = [1:2000];
-ys = perlin1D(2000, 120,500);     % amplitude 40
-y2s = perlin1D(2000, 120,500);  % amplitude 120
-y3s = perlin1D(2000, 80,500);   % amplitude 80
-figure; plot(xs, ys, 'r');
-hold on; plot(xs, y2s, 'b');
-hold on; plot(xs, y3s, 'g');
-axis on;
-
