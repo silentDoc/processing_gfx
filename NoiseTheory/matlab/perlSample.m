1fa8e1022360b4f1fe71914453270efc52b1b3dc
silentDoc <ramon78t@yahoo.es>
2024-03-23 09:15:42 +0100

    Added perlin noise 1D Matlab samples

--- NoiseTheory/matlab/perlSample.m
+++ NoiseTheory/matlab/perlSample.m
@@ -1,10 +1,20 @@
 # perlSample
 % Example usage:
-num_points = 1000;
-persistence = 25;
-num_octaves = 1;
+%num_points = 1000;
+%persistence = 25;
+%num_octaves = 1;
 
-noise = perlinNoise1D(num_points, persistence, num_octaves);
+%noise = perlinNoise1D(num_points, persistence, num_octaves);
+%plot(noise);
+%title('1D Perlin Noise');
+%xlabel('X');
+%ylabel('Noise Value');
+
+% Example usage:
+num_points = 1000;
+frequency = 0.015; % Adjust frequency as needed
+
+noise = pNoise1D(num_points, frequency);
 plot(noise);
 title('1D Perlin Noise');
 xlabel('X');
